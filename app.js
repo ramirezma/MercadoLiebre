4978a310eabbcc29058c2e82eb4f83a571bc184b
ramirezma <ramirezma90@gmail.com>
2022-03-15 22:22:28 -0300

    otro comit

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express ();
 
+
 app.use (express.static ("public"));
 
-app.listen (process.env.PORT || 3030, () => console.log ("Server running")); 
+app.listen (3030, () => console.log ("Server running")); 
 
 app.get ("/", (req, res) => { res.sendFile (path.join (__dirname, "views/home.html")) });
 
